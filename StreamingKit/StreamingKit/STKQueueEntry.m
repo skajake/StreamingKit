3459e5a1299a63849ee2ca13a8d8845d9993c37a
Jakob Stuber <skajake@gmail.com>
2026-03-05 20:31:23 -0600

    Fix warnings

--- StreamingKit/StreamingKit/STKQueueEntry.m
+++ StreamingKit/StreamingKit/STKQueueEntry.m
@@ -18,7 +18,7 @@
 {
     if (self = [super init])
     {
-        self->spinLock = OS_SPINLOCK_INIT;
+        self->spinLock = OS_UNFAIR_LOCK_INIT;
         
         self.dataSource = dataSourceIn;
         self.queueItemId = queueItemIdIn;
@@ -31,11 +31,11 @@
 
 -(void) reset
 {
-    OSSpinLockLock(&self->spinLock);
+    os_unfair_lock_lock(&self->spinLock);
     self->framesQueued = 0;
     self->framesPlayed = 0;
     self->lastFrameQueued = -1;
-    OSSpinLockUnlock(&self->spinLock);
+    os_unfair_lock_unlock(&self->spinLock);
 }
 
 -(double) calculatedBitRate
@@ -109,9 +109,9 @@
 
 -(Float64) progressInFrames
 {
-    OSSpinLockLock(&self->spinLock);
+    os_unfair_lock_lock(&self->spinLock);
     Float64 retval = (self->seekTime + self->audioStreamBasicDescription.mSampleRate) + self->framesPlayed;
-    OSSpinLockUnlock(&self->spinLock);
+    os_unfair_lock_unlock(&self->spinLock);
     
     return retval;
 }
